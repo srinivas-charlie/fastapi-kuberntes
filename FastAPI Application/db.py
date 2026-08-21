6d66fd9af80605b140e156c297fd56779da89630
srinivas <srinivasshaik19@gmail.com>
2026-08-21 11:55:16 +0530

    folders seggregation of files

---
+++ FastAPI Application/db.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+sessionLocal = sessionmaker(bind=engine)
+    
+Base = declarative_base()
+
+
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
---
+++ FastAPI Application/models.py
@@ -0,0 +1,15 @@
+from db import Base
+from sqlalchemy import Column, Integer, String, LargeBinary
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, default="user")
+    profile_photo = Column(LargeBinary, nullable=True)
+
+
